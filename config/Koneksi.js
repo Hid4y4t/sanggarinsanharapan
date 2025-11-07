873d3bb3ecabe541edddd90f11c263de7aaed814
Hidayat <muh.nr.hidayatullah@gmail.com>
2025-11-08 05:52:15 +0700

    d
    g

--- config/Koneksi.js
+++ config/Koneksi.js
@@ -1,8 +1,19 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("u220341190_sanggar", "u220341190_sanggar", "@Hidayatullah23", {
-    host: "srv1200.hstgr.io",
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
     dialect: "mysql",
-});
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+  }
+);
 
 export default db;
