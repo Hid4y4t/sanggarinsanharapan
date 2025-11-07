1ac2a13e8dc6598b425080f1c6bb54c2a3a94264
Hidayat <muh.nr.hidayatullah@gmail.com>
2025-11-08 04:39:26 +0700

    update

--- config/Koneksi.js
+++ config/Koneksi.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("sanggar", "root", "", {
-    host: "localhost",
+const db = new Sequelize("u220341190_sanggar", "u220341190_sanggar", "@Hidayatullah23", {
+    host: "srv1200.hstgr.io",
     dialect: "mysql",
 });
 
